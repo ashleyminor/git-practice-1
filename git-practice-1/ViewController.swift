d0894896fecf7776c3bb9a1a2da162301a0f0525
Ashley Minor <ashleyminor07@gmail.com>
2019-05-17 15:19:44 -0600

    Initial Commit

---
+++ git-practice-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git-practice-1
+//
+//  Created by Ashley Minor on 5/17/19.
+//  Copyright © 2019 Ashley Minor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
